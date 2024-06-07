32a8128f3924c79f62b21ea57363acb3baa64628
Shitong Luo <luost26@gmail.com>
2024-06-07 01:32:34 -0400

    chmod +x

---
+++ scripts/goal_directed/00-run_chemprojector.sh
@@ -0,0 +1,7 @@
+python sample.py \
+    --input data/goal_directed/goal_hard_cwo.csv \
+    --output results/goal_directed/projected.csv \
+    --model-path data/trained_weights/default.ckpt \
+    --num-gpus -1 \
+    --num-workers-per-gpu 2 \
+    --exhaustiveness 64 \
